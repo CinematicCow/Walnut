e274fbaef55f76616fc40ac11aef302ffef4bc12
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2021-10-01 14:37:44 +0530

    refactor: removed isEmpty decorator

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,15 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class LoginUserDto {
   @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
   @IsEmail()
   email: string;
 
   @ApiProperty()
-  @IsNotEmpty()
   @IsString()
   password: string;
 }
